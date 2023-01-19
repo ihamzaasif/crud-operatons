8802b890527d401ddca5a820bf96e6f26500b659
Your Name~Hamza <ihamzaasif1@gmail.com>
2023-01-19 18:50:21 +0500

    modified:   product/product/fixtures/product_data.json 	modified:   product/product/models.py 	modified:   product/product/views.py

--- product/product/tests/test_views.py
+++ product/product/tests/test_views.py
@@ -1,21 +1,46 @@
 from rest_framework.test import APITestCase
 from django.urls import reverse
 from product.models import Product
+import django_filters
+from django_filters import FilterSet, CharFilter
 from django.core.management import call_command
 import json
 
+class ProductFilter(FilterSet):
+    name = CharFilter(lookup_expr='icontains')
+    class Meta:
+        model = Product
+        fields = ['name']
+    
 class TestProductViews(APITestCase):
     fixtures = ['product_data.json']
 
     def setUp(self):
-        Product.objects.create(name="LCD",color="HD", price=4444)
         self.product_id = Product.objects.get(pk=1).pk
+
+    def test_name_filter(self):
+        queryset = Product.objects.all()
+        filtered_queryset = ProductFilter({'name': 'lcd'}, queryset=queryset).qs
+        data1 = filtered_queryset.values()
+        self.assertEquals(data1[0], {'id': 1, 'name': 'LCD', 'color': 'HD', 'price': 4444})
+        
+    def test_color_filter(self):
+        queryset = Product.objects.all()
+        filtered_queryset = ProductFilter({'color': 'hd'}, queryset=queryset).qs
+        data1 = filtered_queryset.values()
+        self.assertEquals(data1[0], {'id': 1, 'name': 'LCD', 'color': 'HD', 'price': 4444})
+
+    def test_combined_filter(self):
+        queryset = Product.objects.all()
+        filtered_queryset = ProductFilter({'name': 'LCD', 'color': 'HD'}, queryset=queryset).qs
+        data1 = filtered_queryset.values()
+        self.assertEquals(data1[0], {'id': 1, 'name': 'LCD', 'color': 'HD', 'price': 4444})
 
     def test_show_product_GET(self):
         response = self.client.get('/')
         data = response.json()
         self.assertEquals(response.status_code, 200)
-        expected_data = [{'id': 1, 'name': 'LCD', 'color': 'HD', 'price': 4444}, {'id': 6, 'name': 'LCD', 'color': 'HD', 'price': 4444}]
+        expected_data = [{'id': 1, 'name': 'LCD', 'color': 'HD', 'price': 4444},{'id': 2, 'name': 'mug', 'color': 'green', 'price': 5555}]
         self.assertEquals(data, expected_data)
 
     def test_post_product(self):
@@ -32,12 +57,12 @@ class TestProductViews(APITestCase):
         #Test scenario for invalid id
         response = self.client.get(f'/6')
         self.assertEquals(response.status_code, 404)
-        self.assertEquals(response.data, {'error': 'Product not found'})
+        self.assertEquals(response.data, {"detail": "Not found."})
 
         #Test scenario for string type
         response = self.client.get(f'/ab')
         self.assertEquals(response.status_code, 404)
-        self.assertEquals(response.data, {'error': 'Invalid string type'})
+        self.assertEquals(response.data, {"detail": "Not found."})
 
     def test_put_product(self):
         data = {'name': 'LCD', 'color': 'HD', 'price': 4444}
@@ -48,12 +73,12 @@ class TestProductViews(APITestCase):
         #Test scenario for invalid id
         response = self.client.put(f'/8', data=json.dumps(data), content_type='application/json')
         self.assertEquals(response.status_code, 404)
-        self.assertEquals(response.data, {'error': 'Product not found'})
+        self.assertEquals(response.data, {"detail": "Not found."})
 
         #Test scenario for String type
         response = self.client.put(f'/ab', data=json.dumps(data), content_type='application/json')
         self.assertEquals(response.status_code, 404)
-        self.assertEquals(response.data, {'error': 'Invalid string type'})
+        self.assertEquals(response.data, {"detail": "Not found."})
 
     def test_delete_product(self):
         response = self.client.delete(f'/{self.product_id}')
@@ -63,9 +88,14 @@ class TestProductViews(APITestCase):
         #Test scenario for invalid id
         response = self.client.delete(f'/8')
         self.assertEquals(response.status_code, 404)
-        self.assertEquals(response.data, {'error': 'Product not found'})
+        self.assertEquals(response.data, {"detail": "Not found."})
 
         #Test scenario for invalid id
         response = self.client.delete(f'/ab')
         self.assertEquals(response.status_code, 404)
-        self.assertEquals(response.data, {'error': 'Invalid string type'})
+        self.assertEquals(response.data, {"detail": "Not found."})
+
+
+
+
+
